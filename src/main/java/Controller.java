8a09c0f43a84ec4e43c60245e79eb5b5d9d7478d
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-07-09 22:40:20 +0100

    Added all vtl and public folder and css

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -7,12 +7,14 @@ import java.util.Collections;
 import java.util.HashMap;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class Controller {
 
     public static void main(String[] args) {
 
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+        staticFileLocation("/public");
 
         ArrayList<Student> students = new ArrayList<Student>();
 
@@ -58,11 +60,65 @@ public class Controller {
         students.add(craig);
         students.add(greg);
 
-        get("/students", (req, res) -> {
+
+
+        get("/random", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
-            model.put("students", students);
-            model.put("template", "students.vtl");
-            model.put("heading", "All Students");
+            model.put("template", "random.vtl");
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+
+        get("/random/one", (req, res) -> {
+            Collections.shuffle(students);
+            Student student = students.get(0);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("student", student);
+            model.put("template", "randomName.vtl");
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+
+        get("/random/two", (req, res) ->{
+            Collections.shuffle(students);
+            Student student1 = students.get(0);
+            Student student2 = students.get(1);
+            ArrayList<Student> twoStudents = new ArrayList<>();
+            twoStudents.add(student1);
+            twoStudents.add(student2);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("twoStudents", twoStudents);
+            model.put("template", "twoRandomNames.vtl");
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+
+        get("/random/three", (seq, res) ->{
+            Collections.shuffle(students);
+            Student student1 = students.get(0);
+            Student student2 = students.get(1);
+            Student student3 = students.get(3);
+            ArrayList<Student> threeStudents = new ArrayList<>();
+            threeStudents.add(student1);
+            threeStudents.add(student2);
+            threeStudents.add(student3);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("threeStudents", threeStudents);
+            model.put("template", "threeRandomNames.vtl");
+            return new ModelAndView(model, "layout.vtl");
+        }, velocityTemplateEngine);
+
+        get("/random/four", (seq, res) ->{
+            Collections.shuffle(students);
+            Student student1 = students.get(0);
+            Student student2 = students.get(1);
+            Student student3 = students.get(3);
+            Student student4 = students.get(4);
+            ArrayList<Student> fourStudents = new ArrayList<>();
+            fourStudents.add(student1);
+            fourStudents.add(student2);
+            fourStudents.add(student3);
+            fourStudents.add(student4);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("fourStudents", fourStudents);
+            model.put("template", "fourRandomNames.vtl");
             return new ModelAndView(model, "layout.vtl");
         }, velocityTemplateEngine);
 
